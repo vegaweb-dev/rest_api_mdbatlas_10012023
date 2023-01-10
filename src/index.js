2973ae2e25b9826757591b3bdc084b4aedbf638c
carlos <carlosjuniorrvega@hotmail.com>
2023-01-10 14:41:28 -0500

    feat:mongodb connection. CV

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 //Set port
 app.set('port', process.env.PORT || 3000);
+
+//Database Connection
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(console.log('connected to database'))
+  .catch((err) => {
+    console.error(err);
+  });
 
 //Start Server
 app.listen(app.get('port'), () => {
