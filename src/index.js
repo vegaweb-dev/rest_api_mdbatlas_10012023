cfba79bf86cbe31a9a97d4f4acc5201d93a68e7c
carlos <carlosjuniorrvega@hotmail.com>
2023-01-10 14:52:40 -0500

    feat:add and test get route. CV

--- src/index.js
+++ src/index.js
@@ -7,6 +7,15 @@ require('dotenv').config();
 //Set port
 app.set('port', process.env.PORT || 3000);
 
+//Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('combined'));
+
+//Routes
+app.get('/', (req, res) => {
+  res.send('sending string through get route');
+});
 //Database Connection
 mongoose
   .connect(process.env.MONGODB_URI)
